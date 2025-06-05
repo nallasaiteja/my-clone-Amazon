602c0db2d764616e0c9e08fb2bba341959d40b2a
nallasaiteja <nallasaitejagupta@gmail.com>
2025-06-06 00:09:34 +0530

    added checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,49 @@
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+cart = [{
+    productId : "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity :2
+},
+{
+    productId : "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity :1
+}
+];
+}
+function saveToStorage(){
+     localStorage.setItem('cart',JSON.stringify(cart));
+}
+
+
+export function addToCart(productId){
+  let matchingItem;
+       cart.forEach((cartitem)=>{
+         if(productId === cartitem.productId){
+            matchingItem = cartitem;
+         }
+       })
+
+       if(matchingItem){
+          matchingItem.quantity += 1;
+       }
+       else{
+        cart.push({
+          productId : productId,
+          quantity : 1
+        })
+      }
+      saveToStorage();
+}
+
+export function removeFromCart(productId){
+    const newArr = [];
+
+    cart.forEach((cartItem)=>{
+       if(cartItem.productId !== productId){
+          newArr.push(cartItem);
+       }
+    })
+
+    cart = newArr;
+    saveToStorage();
+}
